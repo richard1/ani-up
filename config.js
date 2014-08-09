40e7c73be4cc45ee77b65eb50d81398ad937c5f2
Richard Lin <richwierdo@gmail.com>
2014-08-09 01:44:10 -0700

    Add list update functionality through POST

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ var config = {}
 config.MAL_HOST = "myanimelist.net"
 config.MAL_SEARCH = "/api/anime/search.xml?q="
 config.MAL_VERIFY = "/api/account/verify_credentials.xml"
+config.MAL_UPDATE = "/api/animelist/update/"
 
 module.exports = config;
--- mal.js
+++ mal.js
@@ -1,23 +1,32 @@
 var http = require('http');
+var request = require('request');
+var qs = require('querystring');
+
 var config = require('./config');
 var creds = require('./creds');
 
 function verify(username, password) {
-    sendHttpRequest(config.MAL_VERIFY, username, password);
+    sendRequest(config.MAL_VERIFY, username, password);
 }
 
 function search(query, username, password) {
-    sendHttpRequest(config.MAL_SEARCH + query, username, password);
+    sendRequest(config.MAL_SEARCH + query, username, password);
 }
 
-function sendHttpRequest(path, username, password) {
+/**
+ * Sends an HTTP GET request to the given MAL path, authenticating
+ * with the given username and password.
+ *
+ * TODO refactor to use "request"
+ */
+function sendRequest(path, username, password) {
     var options = {
         host: config.MAL_HOST,
         path: path,
         headers: {
-            'user-agent': config.MAL_API_KEY
+            'user-agent': config.MAL_API_KEY,
         },
-        auth: username + ':' + password
+        auth: username + ':' + password,
     };
 
     http.request(options, function(response) {
@@ -25,4 +34,28 @@ function sendHttpRequest(path, username, password) {
     }).end();
 };
 
+function updateList(id, episode, username, password) {
+    var postData = { data: buildAnimeValuesXml(episode) };
+    request.post({
+        uri: "http://" + config.MAL_HOST + config.MAL_UPDATE + id + ".xml",
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded'
+        },
+        body: qs.stringify(postData)
+    }, function(err, res, body){
+        if(err || res.statusCode != 200 || body != "Updated") {
+            console.log("An error occurred while updating your list.");
+        }
+        else {
+            console.log("List successfully updated!");
+        }
+    }).auth(username, password, false);
+}
+
+function buildAnimeValuesXml(episode) {
+    return '<?xml version="1.0" encoding="UTF-8"?><entry><episode>' + 
+        episode + '</episode></entry>';
+}
+
 search('barakamon', creds.MAL_USERNAME, creds.MAL_PASSWORD);    // y'all need to watch this
+updateList(22, 0, creds.MAL_USERNAME, creds.MAL_PASSWORD);
