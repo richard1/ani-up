63303e49f2f7f435dc7b25c7b255647155f577e4
Richard Lin <richwierdo@gmail.com>
2014-08-17 18:28:03 -0700

    Add example creds file for easier setup

---
+++ creds.example.js
@@ -0,0 +1,15 @@
+// TODO rename me to 'creds.js'
+var creds = {}
+
+creds.MAL_API_KEY = "your key here"
+
+creds.MAL_USERNAME = "your username here"
+creds.MAL_PASSWORD = "your password here"
+
+creds.HUMMINGBIRD_API_KEY = "your key here"
+
+creds.HUMMINGBIRD_EMAIL = "your email here"
+creds.HUMMINGBIRD_USERNAME = "your username here"
+creds.HUMMINGBIRD_PASSWORD = "your password here"
+
+module.exports = creds;
